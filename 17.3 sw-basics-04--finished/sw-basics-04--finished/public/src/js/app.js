cc73c4b0a32d7d6424c31137698470bbfa161266
salah elhossiny <salah_elhossiny856@yahoo.com>
2020-07-04 10:07:57 +0200

    pwa add

---
+++ 17.3 sw-basics-04--finished/sw-basics-04--finished/public/src/js/app.js
@@ -0,0 +1,17 @@
+
+var deferredPrompt;
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function() {
+      console.log('Service worker registered!');
+    });
+}
+
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
